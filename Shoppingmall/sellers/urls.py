20c8350850f9ca01b03f7daf6b69f818e2de3fd6
soojin <kimsoojin@gimsujin-ui-MacBookPro.local>
2020-07-13 14:16:57 +0900

    add login of seller

--- Shoppingmall/sellers/urls.py
+++ Shoppingmall/sellers/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('',views.home,name='sellers_home'),
     path('signup',views.signup,name='sellers_signup'),
+    path('login',views.login,name='sellers_login'),
 
 ]
--- Shoppingmall/sellers/views.py
+++ Shoppingmall/sellers/views.py
@@ -9,6 +9,10 @@ from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password,check_password 
 
 
+def home(request):
+#        return HttpResponse( .userID)     
+    return render(request, 'seller_home.html')
+
 def signup(request):   #회원가입 페이지를 보여주기 위한 함수
     if request.method == "GET":
         return render(request, 'seller_signup.html')
@@ -38,3 +42,32 @@ def signup(request):   #회원가입 페이지를 보여주기 위한 함수
             seller.save()
             return redirect('/')
 
+
+def login(request):
+    response_data = {}
+    if request.method == "GET" :
+        return render(request, 'login.html')
+
+    elif request.method == "POST":
+        login_username = request.POST.get('ID', None)
+        login_password = request.POST.get('password', None)
+
+        if not (login_username and login_password):
+            response_data['error']="아이디와 비밀번호를 모두 입력해주세요."
+        else : 
+            myuser = Seller.objects.get(sellerID=login_username) 
+            #db에서 꺼내는 명령. Post로 받아온 username으로 , db의 username을 꺼내온다.
+            if check_password(login_password, myuser.password):
+                request.session['seller'] = myuser.id 
+                #세션도 딕셔너리 변수 사용과 똑같이 사용하면 된다.
+                #세션 user라는 key에 방금 로그인한 id를 저장한것.
+                return redirect('/seller')
+            else:
+                response_data['error'] = "비밀번호를 틀렸습니다."
+
+        return render(request, 'login.html',response_data)
+
+def logout(request):
+    if request.session.get('seller'):
+        del(request.session['seller'])
+    return redirect('/')
