3a850f1f90bcbfe965402abd9204ec61c64d786a
jaewon <jwsims@naver.com>
2020-07-24 00:23:49 +0900

    yes

--- Shoppingmall/Shoppingmall/urls.py
+++ Shoppingmall/Shoppingmall/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('sellers/', include('sellers.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
