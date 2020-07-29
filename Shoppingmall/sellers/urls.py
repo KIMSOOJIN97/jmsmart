ada0c5727687b795295c57cbbe829f35fa1c4788
soojin <kimsoojin@gimsujin-ui-MacBookPro.local>
2020-07-29 14:06:17 +0900

    add count

--- Shoppingmall/sellers/urls.py
+++ Shoppingmall/sellers/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     
-    path('',views.item,name='item'),
+    path('',views.home,name='home'),
+
+    path('item',views.item,name='item'),
 
     path('signup',views.signup,name='sellers_signup'),
     path('login',views.login,name='sellers_login'),
